8afbe989efe4cbdcb6a7cd0b7b3c49484150ee39
St4z <90345087+St4z@users.noreply.github.com>
2021-09-08 16:38:23 -0300

    Add files via upload

---
+++ jason_list.js
@@ -0,0 +1,44 @@
+////////////LIST GIFS////////////////
+
+var modelos = [
+    {
+        type: "simples",
+        model: "01",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "simples",
+        model: "02",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "simples",
+        model: "03",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "simples",
+        model: "04",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "casal",
+        model: "05",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "casal",
+        model: "06",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "casal",
+        model: "07",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "casal",
+        model: "08",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    }
+]
